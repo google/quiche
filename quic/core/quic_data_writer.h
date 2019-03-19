c89c7994877092d0a08d5d46995597fa3c81bbbe
danzh <danzh@google.com>
2019-03-19 16:01:07 -0700

    gfe-relnote: n/a(code clean-up) Remove unnecessary #include in quic_data_writer.h.
    PiperOrigin-RevId: 239288651
    Change-Id: I1ebc1430ad4ee1c53985b6f7f18f89e381da254a

--- quic/core/quic_data_writer.h
+++ quic/core/quic_data_writer.h
@@ -8,7 +8,7 @@
 #include <cstddef>
 #include <cstdint>
 
-#include "net/third_party/quiche/src/quic/core/quic_packets.h"
+#include "net/third_party/quiche/src/quic/core/quic_types.h"
 #include "net/third_party/quiche/src/quic/platform/api/quic_endian.h"
 #include "net/third_party/quiche/src/quic/platform/api/quic_export.h"
 #include "net/third_party/quiche/src/quic/platform/api/quic_string_piece.h"
