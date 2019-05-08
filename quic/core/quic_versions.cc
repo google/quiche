cd0823fc3898722954185f05a67caa8a4af699ab
dschinazi <dschinazi@google.com>
2019-05-07 18:29:07 -0700

    Enable IETF retry support on all TLS versions
    gfe-relnote: protected by disabled flag quic_supports_tls_handshake
    PiperOrigin-RevId: 247130863
    Change-Id: I91c95806107f1c1c41a7fed1266429f37b817d51

--- quic/core/quic_versions.cc
+++ quic/core/quic_versions.cc
@@ -61,7 +61,8 @@ bool ParsedQuicVersion::HasHeaderProtection() const {
 }
 
 bool ParsedQuicVersion::SupportsRetry() const {
-  return transport_version == QUIC_VERSION_99;
+  return transport_version == QUIC_VERSION_99 ||
+         handshake_protocol == PROTOCOL_TLS1_3;
 }
 
 std::ostream& operator<<(std::ostream& os, const ParsedQuicVersion& version) {
