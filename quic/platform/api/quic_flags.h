2cb3a595cd1ec4cec303639667d5a9977346f9b8
dschinazi <dschinazi@google.com>
2019-05-17 02:34:16 -0700

    Prevent include failures of quic_flag_utils
    We've had quiche merge failures due to missing includes because in google3 quic_flags ends up including quic_flag_utils but not in Chromium. This ensures inclusion in the api header to make sure behavior is consistent between google3 and Chromium. This is also useful because it is very common for code that uses flags to also want to use flag counts.
    
    gfe-relnote: n/a, compile-only
    PiperOrigin-RevId: 248687080
    Change-Id: I0a163bb06d4b4a8ecc1747245214f329ba565ba3

--- quic/platform/api/quic_flags.h
+++ quic/platform/api/quic_flags.h
@@ -8,6 +8,7 @@
 #include <string>
 #include <vector>
 
+#include "net/third_party/quiche/src/quic/platform/api/quic_flag_utils.h"
 #include "net/quic/platform/impl/quic_flags_impl.h"
 
 // Define a command-line flag that can be automatically set via
