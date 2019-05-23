71117a7400f584569528a17823199ba2fd3d6dc6
dschinazi <dschinazi@google.com>
2019-05-23 02:57:19 -0700

    Add quic_simplify_stop_waiting to QuicVersionInitializeSupportForIetfDraft
    gfe-relnote: n/a, test-only
    PiperOrigin-RevId: 249610062
    Change-Id: Iac0d3abd6491de90142d02561ef6d845d5a6de37

--- quic/core/quic_versions.cc
+++ quic/core/quic_versions.cc
@@ -428,6 +428,7 @@ void QuicVersionInitializeSupportForIetfDraft(int32_t draft_version) {
   SetQuicReloadableFlag(quic_no_client_conn_ver_negotiation, true);
   SetQuicReloadableFlag(quic_eliminate_static_stream_map_3, true);
   SetQuicReloadableFlag(quic_tolerate_reneging, true);
+  SetQuicReloadableFlag(quic_simplify_stop_waiting, true);
   SetQuicRestartFlag(quic_no_server_conn_ver_negotiation2, true);
   SetQuicRestartFlag(quic_server_drop_version_negotiation, true);
   SetQuicRestartFlag(quic_enable_accept_random_ipn, true);
