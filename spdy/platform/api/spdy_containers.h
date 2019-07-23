89b9af539b5877093cdf7ba22cbff9354b6bf723
fayang <fayang@google.com>
2019-07-23 07:52:49 -0700

    gfe-relnote: Move Http2PriorityWriteScheduler from gfe/gfe2/spdy/ to third_party/spdy/core/ to allow QUIC to use it. No functional change expected, not flag protected.
    Also copy gtl::intrusive_list as SpdyIntrusiveList.
    
    PiperOrigin-RevId: 259535206
    Change-Id: Ie600cc4a403ac90a6302824f98bc5e7fd86fca25

--- spdy/platform/api/spdy_containers.h
+++ spdy/platform/api/spdy_containers.h
@@ -37,6 +37,12 @@ inline size_t SpdyHashStringPair(SpdyStringPiece a, SpdyStringPiece b) {
   return SpdyHashStringPairImpl(a, b);
 }
 
+// Used for maps that are typically small, then it is faster than (for example)
+// hash_map which is optimized for large data sets. SpdySmallMap upgrades itself
+// automatically to a SpdySmallMapImpl-specified map when it runs out of space.
+template <typename Key, typename Value, int Size>
+using SpdySmallMap = SpdySmallMapImpl<Key, Value, Size>;
+
 }  // namespace spdy
 
 #endif  // QUICHE_SPDY_PLATFORM_API_SPDY_CONTAINERS_H_
