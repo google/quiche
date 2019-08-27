8c8f74f710e6da4f8d2079add095be614b6abecb
nharper <nharper@google.com>
2019-08-26 22:23:14 -0700

    Enable simplify stop waiting in quic http EndToEndTests
    gfe-relnote: n/a (test-only change)
    PiperOrigin-RevId: 265611721
    Change-Id: I51bbcab7c05572ddd751e718b99180f415aed142

--- quic/core/http/end_to_end_test.cc
+++ quic/core/http/end_to_end_test.cc
@@ -236,6 +236,7 @@ class EndToEndTest : public QuicTestWithParam<TestParams> {
         support_server_push_(false),
         expected_server_connection_id_length_(kQuicDefaultConnectionIdLength) {
     SetQuicFlag(FLAGS_quic_supports_tls_handshake, true);
+    SetQuicReloadableFlag(quic_simplify_stop_waiting, true);
     client_supported_versions_ = GetParam().client_supported_versions;
     server_supported_versions_ = GetParam().server_supported_versions;
     negotiated_version_ = GetParam().negotiated_version;
