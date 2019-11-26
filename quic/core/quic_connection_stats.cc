e619ef6ff48656669eff552557100802949ea05c
wub <wub@google.com>
2019-11-26 08:59:14 -0800

    gfe-relnote: (n/a) Change member initializer list to default member initializers in QuicConnectionStats. No behavior change, not protected.
    PiperOrigin-RevId: 282575998
    Change-Id: I25fe263a3a9137bfeb79360165eddab3a6ff70c0

--- quic/core/quic_connection_stats.cc
+++ quic/core/quic_connection_stats.cc
@@ -6,56 +6,6 @@
 
 namespace quic {
 
-QuicConnectionStats::QuicConnectionStats()
-    : bytes_sent(0),
-      packets_sent(0),
-      stream_bytes_sent(0),
-      packets_discarded(0),
-      bytes_received(0),
-      packets_received(0),
-      packets_processed(0),
-      stream_bytes_received(0),
-      bytes_retransmitted(0),
-      packets_retransmitted(0),
-      bytes_spuriously_retransmitted(0),
-      packets_spuriously_retransmitted(0),
-      packets_lost(0),
-      slowstart_count(0),
-      slowstart_num_rtts(0),
-      slowstart_packets_sent(0),
-      slowstart_bytes_sent(0),
-      slowstart_packets_lost(0),
-      slowstart_bytes_lost(0),
-      packets_dropped(0),
-      undecryptable_packets_received_before_handshake_complete(0),
-      crypto_retransmit_count(0),
-      loss_timeout_count(0),
-      tlp_count(0),
-      rto_count(0),
-      pto_count(0),
-      min_rtt_us(0),
-      srtt_us(0),
-      max_packet_size(0),
-      max_received_packet_size(0),
-      estimated_bandwidth(QuicBandwidth::Zero()),
-      packets_reordered(0),
-      max_sequence_reordering(0),
-      max_time_reordering_us(0),
-      tcp_loss_events(0),
-      connection_creation_time(QuicTime::Zero()),
-      blocked_frames_received(0),
-      blocked_frames_sent(0),
-      num_connectivity_probing_received(0),
-      retry_packet_processed(false),
-      num_coalesced_packets_received(0),
-      num_coalesced_packets_processed(0),
-      num_ack_aggregation_epochs(0) {}
-
-QuicConnectionStats::QuicConnectionStats(const QuicConnectionStats& other) =
-    default;
-
-QuicConnectionStats::~QuicConnectionStats() {}
-
 std::ostream& operator<<(std::ostream& os, const QuicConnectionStats& s) {
   os << "{ bytes_sent: " << s.bytes_sent;
   os << " packets_sent: " << s.packets_sent;
