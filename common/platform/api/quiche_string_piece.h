83e0444fdfac7160f1b61cc1c36e7fb6e112a1a3
QUICHE team <quiche-dev@google.com>
2019-12-11 06:49:49 -0800

    Copy //third_party/quic string libraries to //third_party/quiche
      * quic_str_cat_lib      -> quiche_str_cat_lib
      * quic_string_piece_lib -> quiche_string_piece_lib
      * quic_text_utils       -> quiche_text_utils
    
    To keep CLs small, a series of followup CLs will modify consumers of
    these libraries to use the quiche variant. A final followup CL will
    remove the originals from //third_party/quic.
    
    The motivation is to expose base64 functions to our HTTPSSVC parsing
    library, which will eventually land in //third_party and be shared
    with Chromium via //third_party/quiche.
    
    PiperOrigin-RevId: 284975905
    Change-Id: I78d0b65f7a3afb5613d8f9de1c98b4e3bfd9ed8e

---
+++ common/platform/api/quiche_string_piece.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef QUICHE_COMMON_PLATFORM_API_QUICHE_STRING_PIECE_H_
+#define QUICHE_COMMON_PLATFORM_API_QUICHE_STRING_PIECE_H_
+
+#include "net/quiche/common/platform/impl/quiche_string_piece_impl.h"
+
+namespace quiche {
+
+using QuicheStringPiece = quiche::QuicheStringPieceImpl;
+
+}  // namespace quiche
+
+#endif  // QUICHE_COMMON_PLATFORM_API_QUICHE_STRING_PIECE_H_
