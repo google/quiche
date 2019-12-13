3304f8dd78dff8296d4981f2dd2a3e0b552721cc
dmcardle <dmcardle@google.com>
2019-12-13 08:54:48 -0800

    Use quiche string libraries in third_party/quic/core (part 2)
    Looks like some more occurrences of the Quic libraries cropped up
    between cl/285401547 and now.
    
    gfe-relnote: n/a, no functional change
    PiperOrigin-RevId: 285408322
    Change-Id: I4ee4a869d5027c2568a299e56fec4a54eec1dfca

--- quic/core/legacy_quic_stream_id_manager_test.cc
+++ quic/core/legacy_quic_stream_id_manager_test.cc
@@ -12,6 +12,7 @@
 #include "net/third_party/quiche/src/quic/platform/api/quic_test.h"
 #include "net/third_party/quiche/src/quic/test_tools/quic_session_peer.h"
 #include "net/third_party/quiche/src/quic/test_tools/quic_test_utils.h"
+#include "net/third_party/quiche/src/common/platform/api/quiche_str_cat.h"
 
 namespace quic {
 namespace test {
@@ -30,7 +31,7 @@ struct TestParams {
 
 // Used by ::testing::PrintToStringParamName().
 std::string PrintToString(const TestParams& p) {
-  return QuicStrCat(
+  return quiche::QuicheStrCat(
       ParsedQuicVersionToString(p.version),
       (p.perspective == Perspective::IS_CLIENT ? "Client" : "Server"));
 }
