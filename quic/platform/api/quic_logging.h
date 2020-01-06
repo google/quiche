6a79c9403441dfe36606ef39da2cf2cb986f8136
dmcardle <dmcardle@google.com>
2020-01-06 10:07:25 -0800

    Delete quic_logging impl, define in terms of quiche_logging
    gfe-relnote: n/a, no functional change
    PiperOrigin-RevId: 288322243
    Change-Id: Ib1c3fb9ffd445b8122048483237761fe7f1eac7d

--- quic/platform/api/quic_logging.h
+++ quic/platform/api/quic_logging.h
@@ -5,36 +5,34 @@
 #ifndef QUICHE_QUIC_PLATFORM_API_QUIC_LOGGING_H_
 #define QUICHE_QUIC_PLATFORM_API_QUIC_LOGGING_H_
 
-#include "net/quic/platform/impl/quic_logging_impl.h"
+#include "net/third_party/quiche/src/common/platform/api/quiche_logging.h"
 
 // Please note following QUIC_LOG are platform dependent:
 // INFO severity can be degraded (to VLOG(1) or DVLOG(1)).
 // Some platforms may not support QUIC_LOG_FIRST_N or QUIC_LOG_EVERY_N_SEC, and
 // they would simply be translated to LOG.
 
-#define QUIC_DVLOG(verbose_level) QUIC_DVLOG_IMPL(verbose_level)
-#define QUIC_DVLOG_IF(verbose_level, condition) \
-  QUIC_DVLOG_IF_IMPL(verbose_level, condition)
-#define QUIC_DLOG(severity) QUIC_DLOG_IMPL(severity)
-#define QUIC_DLOG_IF(severity, condition) QUIC_DLOG_IF_IMPL(severity, condition)
-#define QUIC_VLOG(verbose_level) QUIC_VLOG_IMPL(verbose_level)
-#define QUIC_LOG(severity) QUIC_LOG_IMPL(severity)
-#define QUIC_LOG_FIRST_N(severity, n) QUIC_LOG_FIRST_N_IMPL(severity, n)
-#define QUIC_LOG_EVERY_N_SEC(severity, seconds) \
-  QUIC_LOG_EVERY_N_SEC_IMPL(severity, seconds)
-#define QUIC_LOG_IF(severity, condition) QUIC_LOG_IF_IMPL(severity, condition)
+#define QUIC_DVLOG QUICHE_DVLOG
+#define QUIC_DVLOG_IF QUICHE_DVLOG_IF
+#define QUIC_DLOG QUICHE_DLOG
+#define QUIC_DLOG_IF QUICHE_DLOG_IF
+#define QUIC_VLOG QUICHE_VLOG
+#define QUIC_LOG QUICHE_LOG
+#define QUIC_LOG_FIRST_N QUICHE_LOG_FIRST_N
+#define QUIC_LOG_EVERY_N_SEC QUICHE_LOG_EVERY_N_SEC
+#define QUIC_LOG_IF QUICHE_LOG_IF
 
-#define QUIC_PREDICT_FALSE(x) QUIC_PREDICT_FALSE_IMPL(x)
-#define QUIC_PREDICT_TRUE(x) QUIC_PREDICT_TRUE_IMPL(x)
+#define QUIC_PREDICT_FALSE QUICHE_PREDICT_FALSE
+#define QUIC_PREDICT_TRUE QUICHE_PREDICT_TRUE
 
 // This is a noop in release build.
-#define QUIC_NOTREACHED() QUIC_NOTREACHED_IMPL()
+#define QUIC_NOTREACHED QUICHE_NOTREACHED
 
-#define QUIC_PLOG(severity) QUIC_PLOG_IMPL(severity)
+#define QUIC_PLOG QUICHE_PLOG
 
-#define QUIC_DLOG_INFO_IS_ON() QUIC_DLOG_INFO_IS_ON_IMPL()
-#define QUIC_LOG_INFO_IS_ON() QUIC_LOG_INFO_IS_ON_IMPL()
-#define QUIC_LOG_WARNING_IS_ON() QUIC_LOG_WARNING_IS_ON_IMPL()
-#define QUIC_LOG_ERROR_IS_ON() QUIC_LOG_ERROR_IS_ON_IMPL()
+#define QUIC_DLOG_INFO_IS_ON QUICHE_DLOG_INFO_IS_ON
+#define QUIC_LOG_INFO_IS_ON QUICHE_LOG_INFO_IS_ON
+#define QUIC_LOG_WARNING_IS_ON QUICHE_LOG_WARNING_IS_ON
+#define QUIC_LOG_ERROR_IS_ON QUICHE_LOG_ERROR_IS_ON
 
 #endif  // QUICHE_QUIC_PLATFORM_API_QUIC_LOGGING_H_
