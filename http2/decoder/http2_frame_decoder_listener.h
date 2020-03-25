80b277ae7125317bf1e723d65a2d6005bdc04bec
wub <wub@google.com>
2020-03-25 13:41:17 -0700

    gfe-relnote: (n/a) Make Http2FrameDecoderNoOpListener compile with -Wunused-parameter. No behavior change.
    This is to make it compile in Envoy.
    
    PiperOrigin-RevId: 302959769
    Change-Id: I7afd2492793aa487569eec0248b6eb79ab408f0e

--- http2/decoder/http2_frame_decoder_listener.h
+++ http2/decoder/http2_frame_decoder_listener.h
@@ -302,51 +302,51 @@ class Http2FrameDecoderNoOpListener : public Http2FrameDecoderListener {
   // SpdyFramer's exact states.
   bool OnFrameHeader(const Http2FrameHeader& header) override;
 
-  void OnDataStart(const Http2FrameHeader& header) override {}
-  void OnDataPayload(const char* data, size_t len) override {}
+  void OnDataStart(const Http2FrameHeader& /*header*/) override {}
+  void OnDataPayload(const char* /*data*/, size_t /*len*/) override {}
   void OnDataEnd() override {}
-  void OnHeadersStart(const Http2FrameHeader& header) override {}
-  void OnHeadersPriority(const Http2PriorityFields& priority) override {}
-  void OnHpackFragment(const char* data, size_t len) override {}
+  void OnHeadersStart(const Http2FrameHeader& /*header*/) override {}
+  void OnHeadersPriority(const Http2PriorityFields& /*priority*/) override {}
+  void OnHpackFragment(const char* /*data*/, size_t /*len*/) override {}
   void OnHeadersEnd() override {}
-  void OnPriorityFrame(const Http2FrameHeader& header,
-                       const Http2PriorityFields& priority) override {}
-  void OnContinuationStart(const Http2FrameHeader& header) override {}
+  void OnPriorityFrame(const Http2FrameHeader& /*header*/,
+                       const Http2PriorityFields& /*priority*/) override {}
+  void OnContinuationStart(const Http2FrameHeader& /*header*/) override {}
   void OnContinuationEnd() override {}
-  void OnPadLength(size_t trailing_length) override {}
-  void OnPadding(const char* padding, size_t skipped_length) override {}
-  void OnRstStream(const Http2FrameHeader& header,
-                   Http2ErrorCode error_code) override {}
-  void OnSettingsStart(const Http2FrameHeader& header) override {}
-  void OnSetting(const Http2SettingFields& setting_fields) override {}
+  void OnPadLength(size_t /*trailing_length*/) override {}
+  void OnPadding(const char* /*padding*/, size_t /*skipped_length*/) override {}
+  void OnRstStream(const Http2FrameHeader& /*header*/,
+                   Http2ErrorCode /*error_code*/) override {}
+  void OnSettingsStart(const Http2FrameHeader& /*header*/) override {}
+  void OnSetting(const Http2SettingFields& /*setting_fields*/) override {}
   void OnSettingsEnd() override {}
-  void OnSettingsAck(const Http2FrameHeader& header) override {}
-  void OnPushPromiseStart(const Http2FrameHeader& header,
-                          const Http2PushPromiseFields& promise,
-                          size_t total_padding_length) override {}
+  void OnSettingsAck(const Http2FrameHeader& /*header*/) override {}
+  void OnPushPromiseStart(const Http2FrameHeader& /*header*/,
+                          const Http2PushPromiseFields& /*promise*/,
+                          size_t /*total_padding_length*/) override {}
   void OnPushPromiseEnd() override {}
-  void OnPing(const Http2FrameHeader& header,
-              const Http2PingFields& ping) override {}
-  void OnPingAck(const Http2FrameHeader& header,
-                 const Http2PingFields& ping) override {}
-  void OnGoAwayStart(const Http2FrameHeader& header,
-                     const Http2GoAwayFields& goaway) override {}
-  void OnGoAwayOpaqueData(const char* data, size_t len) override {}
+  void OnPing(const Http2FrameHeader& /*header*/,
+              const Http2PingFields& /*ping*/) override {}
+  void OnPingAck(const Http2FrameHeader& /*header*/,
+                 const Http2PingFields& /*ping*/) override {}
+  void OnGoAwayStart(const Http2FrameHeader& /*header*/,
+                     const Http2GoAwayFields& /*goaway*/) override {}
+  void OnGoAwayOpaqueData(const char* /*data*/, size_t /*len*/) override {}
   void OnGoAwayEnd() override {}
-  void OnWindowUpdate(const Http2FrameHeader& header,
-                      uint32_t increment) override {}
-  void OnAltSvcStart(const Http2FrameHeader& header,
-                     size_t origin_length,
-                     size_t value_length) override {}
-  void OnAltSvcOriginData(const char* data, size_t len) override {}
-  void OnAltSvcValueData(const char* data, size_t len) override {}
+  void OnWindowUpdate(const Http2FrameHeader& /*header*/,
+                      uint32_t /*increment*/) override {}
+  void OnAltSvcStart(const Http2FrameHeader& /*header*/,
+                     size_t /*origin_length*/,
+                     size_t /*value_length*/) override {}
+  void OnAltSvcOriginData(const char* /*data*/, size_t /*len*/) override {}
+  void OnAltSvcValueData(const char* /*data*/, size_t /*len*/) override {}
   void OnAltSvcEnd() override {}
-  void OnUnknownStart(const Http2FrameHeader& header) override {}
-  void OnUnknownPayload(const char* data, size_t len) override {}
+  void OnUnknownStart(const Http2FrameHeader& /*header*/) override {}
+  void OnUnknownPayload(const char* /*data*/, size_t /*len*/) override {}
   void OnUnknownEnd() override {}
-  void OnPaddingTooLong(const Http2FrameHeader& header,
-                        size_t missing_length) override {}
-  void OnFrameSizeError(const Http2FrameHeader& header) override {}
+  void OnPaddingTooLong(const Http2FrameHeader& /*header*/,
+                        size_t /*missing_length*/) override {}
+  void OnFrameSizeError(const Http2FrameHeader& /*header*/) override {}
 };
 
 static_assert(!std::is_abstract<Http2FrameDecoderNoOpListener>(),
