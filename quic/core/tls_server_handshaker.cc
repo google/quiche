1783bc22e4aea98fc023ea9f142218f874066e38
nharper <nharper@google.com>
2020-05-12 17:46:53 -0700

    Add flag count macro for quic_enable_tls_resumption
    PiperOrigin-RevId: 311237264
    Change-Id: Ia60565e8a73d8dc062670484f6a511d1cde1ec50

--- quic/core/tls_server_handshaker.cc
+++ quic/core/tls_server_handshaker.cc
@@ -475,6 +475,7 @@ ssl_ticket_aead_result_t TlsServerHandshaker::SessionTicketOpen(
   memcpy(out, decrypted_session_ticket_.data(),
          decrypted_session_ticket_.size());
   *out_len = decrypted_session_ticket_.size();
+  QUIC_RELOADABLE_FLAG_COUNT(quic_enable_tls_resumption);
 
   return ssl_ticket_aead_success;
 }
