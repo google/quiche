4b3123eaf2efbcd16b4b98cd5b4b75f934617e27
fayang <fayang@google.com>
2020-06-29 11:19:30 -0700

    Deprecate gfe2_reloadable_flag_quic_notify_stream_id_manager_when_disconnected.
    PiperOrigin-RevId: 318853384
    Change-Id: I245eda0db89f1b9a975cc90aefeee50df32437bc

--- quic/core/quic_session.cc
+++ quic/core/quic_session.cc
@@ -917,19 +917,11 @@ void QuicSession::OnStreamClosed(QuicStreamId stream_id) {
     // Stream Id manager has been informed with draining streams.
     return;
   }
-  if (!GetQuicReloadableFlag(quic_notify_stream_id_manager_when_disconnected) &&
-      !connection_->connected()) {
-    // Do not bother informing stream ID manager if connection has been
-    // disconnected.
-    return;
-  }
   if (!VersionHasIetfQuicFrames(transport_version())) {
     stream_id_manager_.OnStreamClosed(
         /*is_incoming=*/IsIncomingStream(stream_id));
   }
-  if (GetQuicReloadableFlag(quic_notify_stream_id_manager_when_disconnected) &&
-      !connection_->connected()) {
-    QUIC_RELOADABLE_FLAG_COUNT(quic_notify_stream_id_manager_when_disconnected);
+  if (!connection_->connected()) {
     return;
   }
   if (IsIncomingStream(stream_id)) {
