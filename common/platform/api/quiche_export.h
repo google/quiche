4ea319b6ff2c1bf38adafdb8c9128ab48d3eb9ee
vasilvv <vasilvv@google.com>
2021-01-29 04:53:47 -0800

    Make QUICHE export use new-style default impl.
    PiperOrigin-RevId: 354510465
    Change-Id: I3b55187e2a2e8af8e5870dc24b608ebb5a5ff865

--- common/platform/api/quiche_export.h
+++ common/platform/api/quiche_export.h
@@ -5,13 +5,17 @@
 #ifndef THIRD_PARTY_QUICHE_PLATFORM_API_QUICHE_EXPORT_H_
 #define THIRD_PARTY_QUICHE_PLATFORM_API_QUICHE_EXPORT_H_
 
-#include "net/quiche/common/platform/impl/quiche_export_impl.h"
+#include "quiche_platform_impl/quiche_export_impl.h"
 
-// quiche_export_impl.h defines the following macros:
-// - QUICHE_EXPORT is not meant to be used.
-// - QUICHE_EXPORT_PRIVATE is meant for QUICHE functionality that is built in
-//   Chromium as part of //net, and not fully contained in headers.
-// - QUICHE_NO_EXPORT is meant for QUICHE functionality that is either fully
-//   defined in a header, or is built in Chromium as part of tests or tools.
+// QUICHE_EXPORT is not meant to be used.
+#define QUICHE_EXPORT QUICHE_EXPORT_IMPL
+
+// QUICHE_EXPORT_PRIVATE is meant for QUICHE functionality that is built in
+// Chromium as part of //net, and not fully contained in headers.
+#define QUICHE_EXPORT_PRIVATE QUICHE_EXPORT_PRIVATE_IMPL
+
+// QUICHE_NO_EXPORT is meant for QUICHE functionality that is either fully
+// defined in a header, or is built in Chromium as part of tests or tools.
+#define QUICHE_NO_EXPORT QUICHE_NO_EXPORT_IMPL
 
 #endif  // THIRD_PARTY_QUICHE_PLATFORM_API_QUICHE_EXPORT_H_
