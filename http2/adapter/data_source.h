1f8d3ffe07a930d3703fe41d81ca1fa1b858d62e
QUICHE team <quiche-dev@google.com>
2021-05-21 13:16:36 -0700

    Adds a DataFrameSource interface, and utilities to convert a DataFrameSource into a nghttp2_data_provider.
    DataFrameSource is closer to the existing Envoy use case, and will be easier to incorporate.
    
    PiperOrigin-RevId: 375150554

--- http2/adapter/data_source.h
+++ http2/adapter/data_source.h
@@ -2,11 +2,34 @@
 #define QUICHE_HTTP2_ADAPTER_DATA_SOURCE_H_
 
 #include <string>
+#include <utility>
 
 #include "absl/strings/string_view.h"
 
 namespace http2 {
 namespace adapter {
+
+// Represents a source of DATA frames for transmission to the peer.
+class DataFrameSource {
+ public:
+  virtual ~DataFrameSource() {}
+
+  static constexpr ssize_t kBlocked = 0;
+  static constexpr ssize_t kError = -1;
+
+  // Returns the number of bytes to send in the next DATA frame, and whether
+  // this frame indicates the end of the data. Returns kBlocked if blocked,
+  // kError on error.
+  virtual std::pair<ssize_t, bool> SelectPayloadLength(size_t max_length) = 0;
+
+  // This method is called with a frame header and a payload length to send. The
+  // source should send or buffer the entire frame.
+  virtual void Send(absl::string_view frame_header, size_t payload_length) = 0;
+
+  // If true, the end of this data source indicates the end of the stream.
+  // Otherwise, this data will be followed by trailers.
+  virtual bool send_fin() const = 0;
+};
 
 // Represents a HTTP message body.
 class DataSource {
