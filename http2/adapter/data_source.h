926813becfa32eabb49a7d4257451083ec96b05b
QUICHE team <quiche-dev@google.com>
2021-07-23 12:06:45 -0700

    Removes unused DataSource interface and implementation.
    PiperOrigin-RevId: 386503864

--- http2/adapter/data_source.h
+++ http2/adapter/data_source.h
@@ -32,45 +32,6 @@ class DataFrameSource {
   virtual bool send_fin() const = 0;
 };
 
-// Represents a HTTP message body.
-class DataSource {
- public:
-  virtual ~DataSource() {}
-
-  enum State {
-    // The source is not done, but cannot currently provide more data.
-    NOT_READY,
-    // The source can provide more data.
-    READY,
-    // The source is done.
-    DONE,
-  };
-
-  State state() const { return state_; }
-
-  // The next range of data provided by this data source.
-  virtual absl::string_view NextData() const = 0;
-
-  // Indicates that |bytes| bytes have been consumed by the caller.
-  virtual void Consume(size_t bytes) = 0;
-
- protected:
-  State state_ = NOT_READY;
-};
-
-// A simple implementation constructible from a string_view or std::string.
-class StringDataSource : public DataSource {
- public:
-  explicit StringDataSource(std::string data);
-
-  absl::string_view NextData() const override;
-  void Consume(size_t bytes) override;
-
- private:
-  const std::string data_;
-  absl::string_view remaining_;
-};
-
 }  // namespace adapter
 }  // namespace http2
 
