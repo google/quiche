244e1941d1389f040c2e043ec1ca7dcc810aa398
wub <wub@google.com>
2021-08-11 13:25:06 -0700

    Internal change
    PiperOrigin-RevId: 390205848

--- quic/core/quic_flags_list.h
+++ quic/core/quic_flags_list.h
@@ -34,7 +34,7 @@ QUIC_FLAG(FLAGS_quic_reloadable_flag_quic_can_send_ack_frequency, true)
 // If true, add missing MaybeUpdateAckTimeout for ack-eliciting frames.
 QUIC_FLAG(FLAGS_quic_reloadable_flag_quic_add_missing_update_ack_timeout, true)
 // If true, allow QuicAlarm to be permanently cancelled.
-QUIC_FLAG(FLAGS_quic_restart_flag_quic_alarm_add_permanent_cancel, false)
+QUIC_FLAG(FLAGS_quic_restart_flag_quic_alarm_add_permanent_cancel, true)
 // If true, allow client to enable BBRv2 on server via connection option \'B2ON\'.
 QUIC_FLAG(FLAGS_quic_reloadable_flag_quic_allow_client_enabled_bbr_v2, false)
 // If true, avoid calling reloadable flags in QuicVersionManager constructor by lazily initializing internal state.
