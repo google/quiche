d721ad8911ad6c773e3a63c878cc5b18e46da8a9
fayang <fayang@google.com>
2022-02-26 11:23:20 -0800

    Use std::string instead of absl::string_view in CryptoBufferMap.
    PiperOrigin-RevId: 431166745

--- quic/test_tools/simple_data_producer.h
+++ quic/test_tools/simple_data_producer.h
@@ -55,7 +55,7 @@ class SimpleDataProducer : public QuicStreamFrameDataProducer {
 
   using CryptoBufferMap =
       absl::flat_hash_map<std::pair<EncryptionLevel, QuicStreamOffset>,
-                          absl::string_view>;
+                          std::string>;
 
   SimpleBufferAllocator allocator_;
 
