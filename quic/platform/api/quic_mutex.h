3120f8fece0462d302a63d402f69d72a8cc550d1
bnc <bnc@google.com>
2022-03-10 14:14:52 -0800

    Move mutex and testvalue from quic/platform/impl to quiche/common/platform/impl.
    I'm lumping these together because these are the only ones in quic/platform that
    have a default implementation.
    
    PiperOrigin-RevId: 433848898

--- quic/platform/api/quic_mutex.h
+++ quic/platform/api/quic_mutex.h
@@ -2,99 +2,30 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(b/178613777): Remove this file.
+
 #ifndef QUICHE_QUIC_PLATFORM_API_QUIC_MUTEX_H_
 #define QUICHE_QUIC_PLATFORM_API_QUIC_MUTEX_H_
 
-// TODO(b/178613777): move into the common QUICHE platform.
-#include "quiche_platform_impl/quic_mutex_impl.h"
+#include "common/platform/api/quiche_mutex.h"
 
-#define QUIC_EXCLUSIVE_LOCKS_REQUIRED QUIC_EXCLUSIVE_LOCKS_REQUIRED_IMPL
-#define QUIC_GUARDED_BY QUIC_GUARDED_BY_IMPL
-#define QUIC_LOCKABLE QUIC_LOCKABLE_IMPL
-#define QUIC_LOCKS_EXCLUDED QUIC_LOCKS_EXCLUDED_IMPL
-#define QUIC_SHARED_LOCKS_REQUIRED QUIC_SHARED_LOCKS_REQUIRED_IMPL
-#define QUIC_EXCLUSIVE_LOCK_FUNCTION QUIC_EXCLUSIVE_LOCK_FUNCTION_IMPL
-#define QUIC_UNLOCK_FUNCTION QUIC_UNLOCK_FUNCTION_IMPL
-#define QUIC_SHARED_LOCK_FUNCTION QUIC_SHARED_LOCK_FUNCTION_IMPL
-#define QUIC_SCOPED_LOCKABLE QUIC_SCOPED_LOCKABLE_IMPL
-#define QUIC_ASSERT_SHARED_LOCK QUIC_ASSERT_SHARED_LOCK_IMPL
+#define QUIC_EXCLUSIVE_LOCKS_REQUIRED QUICHE_EXCLUSIVE_LOCKS_REQUIRED
+#define QUIC_GUARDED_BY QUICHE_GUARDED_BY
+#define QUIC_LOCKABLE QUICHE_LOCKABLE
+#define QUIC_LOCKS_EXCLUDED QUICHE_LOCKS_EXCLUDED
+#define QUIC_SHARED_LOCKS_REQUIRED QUICHE_SHARED_LOCKS_REQUIRED
+#define QUIC_EXCLUSIVE_LOCK_FUNCTION QUICHE_EXCLUSIVE_LOCK_FUNCTION
+#define QUIC_UNLOCK_FUNCTION QUICHE_UNLOCK_FUNCTION
+#define QUIC_SHARED_LOCK_FUNCTION QUICHE_SHARED_LOCK_FUNCTION
+#define QUIC_SCOPED_LOCKABLE QUICHE_SCOPED_LOCKABLE
+#define QUIC_ASSERT_SHARED_LOCK QUICHE_ASSERT_SHARED_LOCK
 
 namespace quic {
 
-// A class representing a non-reentrant mutex in QUIC.
-class QUIC_LOCKABLE QUIC_EXPORT_PRIVATE QuicMutex {
- public:
-  QuicMutex() = default;
-  QuicMutex(const QuicMutex&) = delete;
-  QuicMutex& operator=(const QuicMutex&) = delete;
-
-  // Block until this Mutex is free, then acquire it exclusively.
-  void WriterLock() QUIC_EXCLUSIVE_LOCK_FUNCTION();
-
-  // Release this Mutex. Caller must hold it exclusively.
-  void WriterUnlock() QUIC_UNLOCK_FUNCTION();
-
-  // Block until this Mutex is free or shared, then acquire a share of it.
-  void ReaderLock() QUIC_SHARED_LOCK_FUNCTION();
-
-  // Release this Mutex. Caller could hold it in shared mode.
-  void ReaderUnlock() QUIC_UNLOCK_FUNCTION();
-
-  // Returns immediately if current thread holds the Mutex in at least shared
-  // mode.  Otherwise, may report an error (typically by crashing with a
-  // diagnostic), or may return immediately.
-  void AssertReaderHeld() const QUIC_ASSERT_SHARED_LOCK();
-
- private:
-  QuicLockImpl impl_;
-};
-
-// A helper class that acquires the given QuicMutex shared lock while the
-// QuicReaderMutexLock is in scope.
-class QUIC_SCOPED_LOCKABLE QUIC_EXPORT_PRIVATE QuicReaderMutexLock {
- public:
-  explicit QuicReaderMutexLock(QuicMutex* lock) QUIC_SHARED_LOCK_FUNCTION(lock);
-  QuicReaderMutexLock(const QuicReaderMutexLock&) = delete;
-  QuicReaderMutexLock& operator=(const QuicReaderMutexLock&) = delete;
-
-  ~QuicReaderMutexLock() QUIC_UNLOCK_FUNCTION();
-
- private:
-  QuicMutex* const lock_;
-};
-
-// A helper class that acquires the given QuicMutex exclusive lock while the
-// QuicWriterMutexLock is in scope.
-class QUIC_SCOPED_LOCKABLE QUIC_EXPORT_PRIVATE QuicWriterMutexLock {
- public:
-  explicit QuicWriterMutexLock(QuicMutex* lock)
-      QUIC_EXCLUSIVE_LOCK_FUNCTION(lock);
-  QuicWriterMutexLock(const QuicWriterMutexLock&) = delete;
-  QuicWriterMutexLock& operator=(const QuicWriterMutexLock&) = delete;
-
-  ~QuicWriterMutexLock() QUIC_UNLOCK_FUNCTION();
-
- private:
-  QuicMutex* const lock_;
-};
-
-// A Notification allows threads to receive notification of a single occurrence
-// of a single event.
-class QUIC_EXPORT_PRIVATE QuicNotification {
- public:
-  QuicNotification() = default;
-  QuicNotification(const QuicNotification&) = delete;
-  QuicNotification& operator=(const QuicNotification&) = delete;
-
-  bool HasBeenNotified() { return impl_.HasBeenNotified(); }
-
-  void Notify() { impl_.Notify(); }
-
-  void WaitForNotification() { impl_.WaitForNotification(); }
-
- private:
-  QuicNotificationImpl impl_;
-};
+using QuicMutex = quiche::QuicheMutex;
+using QuicReaderMutexLock = quiche::QuicheReaderMutexLock;
+using QuicWriterMutexLock = quiche::QuicheWriterMutexLock;
+using QuicNotification = quiche::QuicheNotification;
 
 }  // namespace quic
 
--- quic/platform/api/quic_testvalue.h
+++ quic/platform/api/quic_testvalue.h
@@ -2,24 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(b/178613777): Remove this file.
+
 #ifndef QUICHE_QUIC_PLATFORM_API_QUIC_TESTVALUE_H_
 #define QUICHE_QUIC_PLATFORM_API_QUIC_TESTVALUE_H_
 
 #include "absl/strings/string_view.h"
-
-// TODO(b/178613777): move into the common QUICHE platform.
-#include "quiche_platform_impl/quic_testvalue_impl.h"
+#include "common/platform/api/quiche_testvalue.h"
 
 namespace quic {
 
-// Interface allowing injection of test-specific code in production codepaths.
-// |label| is an arbitrary value identifying the location, and |var| is a
-// pointer to the value to be modified.
-//
-// Note that this method does nothing in Chromium.
 template <class T>
 void AdjustTestValue(absl::string_view label, T* var) {
-  AdjustTestValueImpl(label, var);
+  quiche::AdjustTestValue(label, var);
 }
 
 }  // namespace quic
