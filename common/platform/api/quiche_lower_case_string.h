b454d36994bc5f1b6794a7973858f8aa7ead654b
rch <rch@google.com>
2022-04-05 15:36:56 -0700

    Convert quiche_lower_case_string_impl.h over to the new-style #include path.
    PiperOrigin-RevId: 439693303

--- common/platform/api/quiche_lower_case_string.h
+++ common/platform/api/quiche_lower_case_string.h
@@ -5,7 +5,7 @@
 #ifndef QUICHE_COMMON_PLATFORM_API_QUICHE_LOWER_CASE_STRING_H_
 #define QUICHE_COMMON_PLATFORM_API_QUICHE_LOWER_CASE_STRING_H_
 
-#include "net/quiche/common/platform/impl/quiche_lower_case_string_impl.h"
+#include "quiche_platform_impl/quiche_lower_case_string_impl.h"
 
 namespace quiche {
 
