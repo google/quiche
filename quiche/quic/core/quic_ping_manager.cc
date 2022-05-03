fdd86bf1ae343ad46bbe7be9fc8b7e6bb3bc8958
fayang <fayang@google.com>
2022-05-03 07:32:10 -0700

    Log more info when retransmittable-on-wire deadline is in the past.
    PiperOrigin-RevId: 446189500

--- quiche/quic/core/quic_ping_manager.cc
+++ quiche/quic/core/quic_ping_manager.cc
@@ -136,7 +136,11 @@ void QuicPingManager::UpdateDeadlines(QuicTime now, bool should_keep_alive,
     // Alarm is set to an earlier time. Do not postpone it.
     QUIC_BUG_IF(quic_retransmittable_on_wire_deadline_is_in_past,
                 retransmittable_on_wire_deadline_ < now)
-        << "QUIC retransmittable PING deadline is in past";
+        << "QUIC retransmittable PING deadline is in past, now: "
+        << now.ToDebuggingValue() << ", retransmittable_on_wire_deadline: "
+        << retransmittable_on_wire_deadline_.ToDebuggingValue()
+        << ", difference: "
+        << (now - retransmittable_on_wire_deadline_).ToDebuggingValue();
     return;
   }
   retransmittable_on_wire_deadline_ = now + retransmittable_on_wire_timeout;
