ce1352545bf94e572e76afed3e72d64a2198436f
martinduke <martinduke@google.com>
2022-11-29 12:16:13 -0800

    Deprecate gfe2_restart_flag_quic_use_hash_quic_codepoint.
    PiperOrigin-RevId: 491705251

--- quiche/quic/core/quic_flags_list.h
+++ quiche/quic/core/quic_flags_list.h
@@ -7,8 +7,6 @@
 #ifdef QUIC_FLAG
 
 QUIC_FLAG(quic_restart_flag_quic_offload_pacing_to_usps2, false)
-
-QUIC_FLAG(quic_restart_flag_quic_use_hash_quic_codepoint, true)
 // A testonly reloadable flag that will always default to false.
 QUIC_FLAG(quic_reloadable_flag_quic_testonly_default_false, false)
 // A testonly reloadable flag that will always default to true.
