5db6dfe1a7ec90cb95d30c0439335717c7bc3f83
haoyuewang <haoyuewang@google.com>
2023-03-22 14:10:12 -0700

    Internal change
    PiperOrigin-RevId: 518669284

--- quiche/quic/core/quic_flags_list.h
+++ quiche/quic/core/quic_flags_list.h
@@ -54,7 +54,7 @@ QUIC_FLAG(quic_reloadable_flag_quic_enable_server_on_wire_ping, true)
 // If true, flush pending frames as well as pending padding bytes on connection migration.
 QUIC_FLAG(quic_reloadable_flag_quic_flush_pending_frames_and_padding_bytes_on_migration, true)
 // If true, ietf connection migration is no longer conditioned on connection option RVCM.
-QUIC_FLAG(quic_reloadable_flag_quic_remove_connection_migration_connection_option_v2, false)
+QUIC_FLAG(quic_reloadable_flag_quic_remove_connection_migration_connection_option_v2, true)
 // If true, include stream information in idle timeout connection close detail.
 QUIC_FLAG(quic_reloadable_flag_quic_add_stream_info_to_idle_close_detail, true)
 // If true, quic server will send ENABLE_CONNECT_PROTOCOL setting and and endpoint will validate required request/response headers and extended CONNECT mechanism and update code counts of valid/invalid headers.
