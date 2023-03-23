09a54d2f17d18b9bb8fe92b169354df2c50ba9ef
ericorth <ericorth@google.com>
2023-03-23 12:11:46 -0700

    Add a fallthrough case to QuicStreamPriority::Default()
    Some Envoy compilations don't like this no-default switch statement, so add a QUICHE_BUG() to handle the case that no type matches.
    
    Also add default copts to the quic_stream_priority build rules in an attempt to get QUICHE builds to match the Envoy failures, but it doesn't seem to make a difference.
    
    PiperOrigin-RevId: 518929051

--- quiche/quic/core/quic_stream_priority.h
+++ quiche/quic/core/quic_stream_priority.h
@@ -84,6 +84,11 @@ class QUICHE_EXPORT QuicStreamPriority {
       case QuicPriorityType::kWebTransport:
         return QuicStreamPriority(WebTransportStreamPriority());
     }
+
+    QUICHE_BUG(unhandled_quic_priority_type_518918225)
+        << "Tried to create QuicStreamPriority for unknown QuicPriorityType "
+        << type;
+    return QuicStreamPriority(HttpStreamPriority());
   }
 
   QuicPriorityType type() const { return absl::visit(TypeExtractor(), value_); }
