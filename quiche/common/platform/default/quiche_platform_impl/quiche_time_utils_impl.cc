fc95ef694198fe6cafb482086414fa9406be1bae
QUICHE team <quiche-dev@google.com>
2023-06-22 16:06:15 -0700

    Make quiche's default time conversion use BoringSSL's functions.
    This is in line with what chrome is now doing, and once this
    lands in chrome we can remove chrome's specific file.
    
    PiperOrigin-RevId: 542693850

--- quiche/common/platform/default/quiche_platform_impl/quiche_time_utils_impl.cc
+++ quiche/common/platform/default/quiche_platform_impl/quiche_time_utils_impl.cc
@@ -4,45 +4,42 @@
 
 #include "quiche_platform_impl/quiche_time_utils_impl.h"
 
-#include "absl/time/civil_time.h"
-#include "absl/time/time.h"
+#include "openssl/time.h"
 
 namespace quiche {
 
-namespace {
-absl::optional<int64_t> QuicheUtcDateTimeToUnixSecondsInner(int year, int month,
-                                                            int day, int hour,
-                                                            int minute,
-                                                            int second) {
-  const absl::CivilSecond civil_time(year, month, day, hour, minute, second);
-  if (second != 60 &&
-      (civil_time.year() != year || civil_time.month() != month ||
-       civil_time.day() != day || civil_time.hour() != hour ||
-       civil_time.minute() != minute || civil_time.second() != second)) {
-    return absl::nullopt;
-  }
-
-  const absl::Time time = absl::FromCivil(civil_time, absl::UTCTimeZone());
-  return absl::ToUnixSeconds(time);
-}
-}  // namespace
-
+// Chrome converts broken out UTC times for certificates to unix times using
+// the BoringSSL routines.
 absl::optional<int64_t> QuicheUtcDateTimeToUnixSecondsImpl(int year, int month,
                                                            int day, int hour,
                                                            int minute,
                                                            int second) {
-  // Handle leap seconds without letting any other irregularities happen.
-  if (second == 60) {
-    auto previous_second = QuicheUtcDateTimeToUnixSecondsInner(
-        year, month, day, hour, minute, second - 1);
-    if (!previous_second.has_value()) {
+  struct tm tmp_tm;
+  tmp_tm.tm_year = year - 1900;
+  tmp_tm.tm_mon = month - 1;
+  tmp_tm.tm_mday = day;
+  tmp_tm.tm_hour = hour;
+  tmp_tm.tm_min = minute;
+  tmp_tm.tm_sec = second;
+  // BoringSSL POSIX time, like POSIX itself, does not support leap seconds.
+  bool leap_second = false;
+  if (tmp_tm.tm_sec == 60) {
+    tmp_tm.tm_sec = 59;
+    leap_second = true;
+  }
+  int64_t result;
+  if (!OPENSSL_tm_to_posix(&tmp_tm, &result)) {
+    return absl::nullopt;
+  }
+  // Our desired behaviour is to return the following second for a leap second
+  // assuming it is a valid time.
+  if (leap_second) {
+    if (!OPENSSL_posix_to_tm(result + 1, &tmp_tm)) {
       return absl::nullopt;
     }
-    return *previous_second + 1;
+    result++;
   }
-
-  return QuicheUtcDateTimeToUnixSecondsInner(year, month, day, hour, minute,
-                                             second);
+  return result;
 }
 
 }  // namespace quiche
