e96fd313996958715fd6378b2b60c69d0271bd4a
danzh <danzh@google.com>
2023-08-08 07:50:57 -0700

    Internal change
    PiperOrigin-RevId: 554823037

--- quiche/quic/core/quic_flags_list.h
+++ quiche/quic/core/quic_flags_list.h
@@ -24,7 +24,7 @@ QUIC_FLAG(quic_reloadable_flag_quic_allow_host_header_in_response, true)
 // If true, QUIC BBR2 will ignore non-positive RTT samples.
 QUIC_FLAG(quic_reloadable_flag_quic_bbr2_ignore_bad_rtt_sample, true)
 // If true, QUIC server will not respond to gQUIC probing packet(PING + PADDING) but treat it as a regular packet.
-QUIC_FLAG(quic_reloadable_flag_quic_ignore_gquic_probing, false)
+QUIC_FLAG(quic_reloadable_flag_quic_ignore_gquic_probing, true)
 // If true, QUIC will default enable MTU discovery at server, with a target of 1450 bytes.
 QUIC_FLAG(quic_reloadable_flag_quic_enable_mtu_discovery_at_server, false)
 // If true, QuicGsoBatchWriter will support release time if it is available and the process has the permission to do so.
