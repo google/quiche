97fba4551b7a3a24ac51fcc2c0f2615304b7c704
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-10-17 07:50:19 -0400

    spelling: disallowed
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- quiche/quic/tools/connect_udp_tunnel.cc
+++ quiche/quic/tools/connect_udp_tunnel.cc
@@ -206,7 +206,7 @@ void ConnectUdpTunnel::OpenTunnel(
     return;
   }
 
-  // TODO(ericorth): Validate that the IP address doesn't fall into diallowed
+  // TODO(ericorth): Validate that the IP address doesn't fall into disallowed
   // ranges per RFC 9298, Section 7.
   QuicSocketAddress address = tools::LookupAddress(AF_UNSPEC, target.value());
   if (!address.IsInitialized()) {
