3b2e6699cd6328b43e96a5c68c46ea8703ba5b38
renjietang <renjietang@google.com>
2023-10-17 15:14:25 -0700

    Default implement QUICHE_PEER_BUG to log the condition too.
    PiperOrigin-RevId: 574278720

--- quiche/common/platform/default/quiche_platform_impl/quiche_bug_tracker_impl.h
+++ quiche/common/platform/default/quiche_platform_impl/quiche_bug_tracker_impl.h
@@ -10,7 +10,8 @@
 #define QUICHE_BUG_IMPL(b) QUICHE_LOG(DFATAL) << #b ": "
 #define QUICHE_BUG_IF_IMPL(b, condition) \
   QUICHE_LOG_IF(DFATAL, condition) << #b ": "
-#define QUICHE_PEER_BUG_IMPL(b) QUICHE_LOG(DFATAL)
-#define QUICHE_PEER_BUG_IF_IMPL(b, condition) QUICHE_LOG_IF(DFATAL, condition)
+#define QUICHE_PEER_BUG_IMPL(b) QUICHE_LOG(DFATAL) << #b ": "
+#define QUICHE_PEER_BUG_IF_IMPL(b, condition) \
+  QUICHE_LOG_IF(DFATAL, condition) << #b ": "
 
 #endif  // QUICHE_COMMON_PLATFORM_DEFAULT_QUICHE_PLATFORM_IMPL_QUICHE_BUG_TRACKER_IMPL_H_
