518b09c790a5a72f0a747ff6b03c1ffd13bf98c6
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-10-17 07:05:49 -0400

    spelling: blackhole
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- quiche/quic/core/quic_flags_list.h
+++ quiche/quic/core/quic_flags_list.h
@@ -55,7 +55,7 @@ QUIC_FLAG(quic_reloadable_flag_quic_allow_client_enabled_bbr_v2, true)
 QUIC_FLAG(quic_restart_flag_quic_opport_bundle_qpack_decoder_data, false)
 // If true, an endpoint does not detect path degrading or blackholing until handshake gets confirmed.
 QUIC_FLAG(quic_reloadable_flag_quic_no_path_degrading_before_handshake_confirmed, true)
-// If true, default-enable 5RTO blachole detection.
+// If true, default-enable 5RTO blackhole detection.
 QUIC_FLAG(quic_reloadable_flag_quic_default_enable_5rto_blackhole_detection2, true)
 // If true, disable QUIC version Q046.
 QUIC_FLAG(quic_reloadable_flag_quic_disable_version_q046, true)
