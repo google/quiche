5f7c70d3291ecfbc9ffe07bea8f6cbe55568389f
QUICHE team <quiche-dev@google.com>
2024-01-11 07:00:26 -0800

    Fix -Wc++11-narrowing-const-reference before re-enabling the warning globally.
    Examples of compilation errors before this CL (needs cl/597308552): http://sponge/0a244b7c-c05b-4fad-bfcf-f92a0b44d10d
    
    Tested:
        TAP --sample ran all affected tests and none failed
        http://test/OCL:597537317:BASE:597503996:1704983090055:81cd8489
    PiperOrigin-RevId: 597544603

--- quiche/quic/core/batch_writer/quic_batch_writer_buffer_test.cc
+++ quiche/quic/core/batch_writer/quic_batch_writer_buffer_test.cc
@@ -108,9 +108,9 @@ TEST_F(QuicBatchWriterBufferTest, InPlacePushes) {
   std::vector<BufferSizeSequence> buffer_size_sequences = {
       // Push large writes until the buffer is near full, then switch to 1-byte
       // writes. This covers the edge cases when detecting insufficient buffer.
-      BufferSizeSequence({{{1350}, kBatchBufferSize - 3000}, {{1}, 1e6}}),
+      BufferSizeSequence({{{1350}, kBatchBufferSize - 3000}, {{1}, 1000000}}),
       // A sequence that looks real.
-      BufferSizeSequence({{{1, 39, 97, 150, 1350, 1350, 1350, 1350}, 1e6}}),
+      BufferSizeSequence({{{1, 39, 97, 150, 1350, 1350, 1350, 1350}, 1000000}}),
   };
 
   for (auto& buffer_size_sequence : buffer_size_sequences) {
