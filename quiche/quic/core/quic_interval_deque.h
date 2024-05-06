c1af894e0f5c4f732a983e7c93227854e203570e
QUICHE team <quiche-dev@google.com>
2024-05-06 00:24:25 -0700

    Fix QuicIntervalDeque::Iterator::operator+=
    The code was referring to a member which didn't exist. This was uncovered by a
    recent Clang change [1] which made the compiler lookup these names at template
    parse time rather than instantiation time.
    
     1. https://github.com/llvm/llvm-project/pull/90152
    
    PiperOrigin-RevId: 630952202

--- quiche/quic/core/quic_interval_deque.h
+++ quiche/quic/core/quic_interval_deque.h
@@ -198,12 +198,12 @@ class QUICHE_NO_EXPORT QuicIntervalDeque {
     Iterator operator+(difference_type amount) const {
       Iterator copy = *this;
       copy.index_ += amount;
-      QUICHE_DCHECK(copy.index_ < copy.deque_->size());
+      QUICHE_DCHECK(copy.index_ < copy.deque_->Size());
       return copy;
     }
     Iterator& operator+=(difference_type amount) {
       index_ += amount;
-      QUICHE_DCHECK(index_ < deque_->size());
+      QUICHE_DCHECK(index_ < deque_->Size());
       return *this;
     }
     difference_type operator-(const Iterator& rhs) const {
