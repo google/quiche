d5b81ed898ce5ab29274ef7133b243bc6a05be73
vasilvv <vasilvv@google.com>
2024-07-11 11:23:18 -0700

    Implement MoQT priority calculation.
    The scheme implements priority assignment that follows the semantics of https://github.com/moq-wg/moq-transport/pull/470 and is compatible with how WebTransport scheduling works.
    
    PiperOrigin-RevId: 651476056

---
+++ quiche/quic/moqt/moqt_priority_test.cc
@@ -0,0 +1,59 @@
+// Copyright 2024 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "quiche/quic/moqt/moqt_priority.h"
+
+#include "quiche/common/platform/api/quiche_test.h"
+
+namespace moqt {
+namespace {
+
+TEST(MoqtPrioirtyTest, TrackPriorities) {
+  // MoQT track priorities are descending (0 is highest), but WebTransport send
+  // order is ascending.
+  EXPECT_GT(SendOrderForStream(0x10, 0x80, 0, MoqtDeliveryOrder::kAscending),
+            SendOrderForStream(0x80, 0x80, 0, MoqtDeliveryOrder::kAscending));
+  EXPECT_GT(SendOrderForStream(0x80, 0x10, 0, MoqtDeliveryOrder::kAscending),
+            SendOrderForStream(0x80, 0x80, 0, MoqtDeliveryOrder::kAscending));
+  // Subscriber priority takes precedence over the sender priority.
+  EXPECT_GT(SendOrderForStream(0x10, 0x80, 0, MoqtDeliveryOrder::kAscending),
+            SendOrderForStream(0x80, 0x10, 0, MoqtDeliveryOrder::kAscending));
+  // Test extreme priority values (0x00 and 0xff).
+  EXPECT_GT(SendOrderForStream(0x00, 0x80, 0, MoqtDeliveryOrder::kAscending),
+            SendOrderForStream(0xff, 0x80, 0, MoqtDeliveryOrder::kAscending));
+  EXPECT_GT(SendOrderForStream(0x80, 0x00, 0, MoqtDeliveryOrder::kAscending),
+            SendOrderForStream(0x80, 0xff, 0, MoqtDeliveryOrder::kAscending));
+}
+
+TEST(MoqtPrioirtyTest, ControlStream) {
+  EXPECT_GT(kMoqtControlStreamSendOrder,
+            SendOrderForStream(0x00, 0x00, 0, MoqtDeliveryOrder::kAscending));
+}
+
+TEST(MoqtPriorityTest, StreamPerGroup) {
+  EXPECT_GT(SendOrderForStream(0x80, 0x80, 0, MoqtDeliveryOrder::kAscending),
+            SendOrderForStream(0x80, 0x80, 1, MoqtDeliveryOrder::kAscending));
+  EXPECT_GT(SendOrderForStream(0x80, 0x80, 1, MoqtDeliveryOrder::kDescending),
+            SendOrderForStream(0x80, 0x80, 0, MoqtDeliveryOrder::kDescending));
+}
+
+TEST(MoqtPriorityTest, StreamPerObject) {
+  // Objects within the same group.
+  EXPECT_GT(
+      SendOrderForStream(0x80, 0x80, 0, 0, MoqtDeliveryOrder::kAscending),
+      SendOrderForStream(0x80, 0x80, 0, 1, MoqtDeliveryOrder::kAscending));
+  EXPECT_GT(
+      SendOrderForStream(0x80, 0x80, 0, 0, MoqtDeliveryOrder::kDescending),
+      SendOrderForStream(0x80, 0x80, 0, 1, MoqtDeliveryOrder::kDescending));
+  // Objects of different groups.
+  EXPECT_GT(
+      SendOrderForStream(0x80, 0x80, 0, 1, MoqtDeliveryOrder::kAscending),
+      SendOrderForStream(0x80, 0x80, 1, 0, MoqtDeliveryOrder::kAscending));
+  EXPECT_GT(
+      SendOrderForStream(0x80, 0x80, 1, 1, MoqtDeliveryOrder::kDescending),
+      SendOrderForStream(0x80, 0x80, 0, 0, MoqtDeliveryOrder::kDescending));
+}
+
+}  // namespace
+}  // namespace moqt
