e613497c482b933a55e809b5cc2b6adb6b642b95
birenroy <birenroy@google.com>
2024-07-12 07:49:31 -0700

    Migrates all users of spdy::Http2HeaderBlock to quiche::HttpHeaderBlock.
    The former is merely an alias for the latter, so this is not a functional change.
    
    Tested:
        TAP for global presubmit queue
        http://test/OCL:651173256:BASE:651464657:1720792960858:9da995fe
    
    Protected by refactoring, no functional change, not protected.
    
    PiperOrigin-RevId: 651771955

--- quiche/quic/moqt/tools/moqt_client.cc
+++ quiche/quic/moqt/tools/moqt_client.cc
@@ -22,8 +22,8 @@
 #include "quiche/quic/tools/quic_default_client.h"
 #include "quiche/quic/tools/quic_event_loop_tools.h"
 #include "quiche/quic/tools/quic_name_lookup.h"
+#include "quiche/common/http/http_header_block.h"
 #include "quiche/common/platform/api/quiche_logging.h"
-#include "quiche/spdy/core/http2_header_block.h"
 
 namespace moqt {
 
@@ -75,7 +75,7 @@ absl::Status MoqtClient::ConnectInner(std::string path,
   }
   spdy_client_.set_store_response(true);
 
-  spdy::Http2HeaderBlock headers;
+  quiche::HttpHeaderBlock headers;
   headers[":scheme"] = "https";
   headers[":authority"] = spdy_client_.server_id().host();
   headers[":path"] = path;
