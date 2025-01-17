16535679768274d3061f1b991082a6a0c8fd1aac
dschinazi <dschinazi@google.com>
2025-01-16 17:54:44 -0800

    Improve QUIC_BUG in GenerateRemainingCryptoFrames
    Add logging to help debug b/390382082.
    
    PiperOrigin-RevId: 716446183

--- quiche/quic/core/quic_packet_creator.cc
+++ quiche/quic/core/quic_packet_creator.cc
@@ -1555,13 +1555,19 @@ size_t QuicPacketCreator::GenerateRemainingCryptoFrames(
       // The only pending data in the packet is non-retransmittable frames.
       // I'm assuming here that they won't occupy so much of the packet that a
       // CRYPTO frame won't fit.
-      QUIC_BUG_IF(quic_bug_10752_26, !HasSoftMaxPacketLength()) << absl::StrCat(
-          ENDPOINT, "Failed to ConsumeCryptoData at level ", level,
-          ", pending_frames: ", GetPendingFramesInfo(),
-          ", has_soft_max_packet_length: ", HasSoftMaxPacketLength(),
-          ", max_packet_length: ", max_packet_length_, ", transmission_type: ",
-          TransmissionTypeToString(next_transmission_type_),
-          ", packet_number: ", packet_number().ToString());
+      QUIC_BUG_IF(GenerateRemainingCryptoFrames_failed_to_consume,
+                  !HasSoftMaxPacketLength())
+          << absl::StrCat(
+                 ENDPOINT, "Failed to ConsumeCryptoData at level ",
+                 EncryptionLevelToString(level),
+                 ", write_length: ", write_length, ", offset: ", offset,
+                 ", BytesFree(): ", BytesFree(),
+                 ", pending_frames: ", GetPendingFramesInfo(),
+                 ", has_soft_max_packet_length: ", HasSoftMaxPacketLength(),
+                 ", max_packet_length: ", max_packet_length_,
+                 ", transmission_type: ",
+                 TransmissionTypeToString(next_transmission_type_),
+                 ", packet_number: ", packet_number().ToString());
       return 0;
     }
     total_bytes_consumed += frame.crypto_frame->data_length;
