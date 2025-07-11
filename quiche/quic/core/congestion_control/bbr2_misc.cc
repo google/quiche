a321d386866ca3297461d96e58896e8de990020b
danzh <danzh@google.com>
2025-07-11 12:57:33 -0700

    Demote QUIC_BUG(quic_bbr2_prior_in_flight_too_small) to ERROR log.
    This line swamped the alerts of other Envoy users.
    
    PiperOrigin-RevId: 782080499

--- quiche/quic/core/congestion_control/bbr2_misc.cc
+++ quiche/quic/core/congestion_control/bbr2_misc.cc
@@ -156,11 +156,11 @@ void Bbr2NetworkModel::OnCongestionEventStart(
         congestion_event->prior_bytes_in_flight -
         congestion_event->bytes_acked - congestion_event->bytes_lost;
   } else {
-    QUIC_BUG(quic_bbr2_prior_in_flight_too_small)
-        << "prior_bytes_in_flight:" << congestion_event->prior_bytes_in_flight
-        << " is smaller than the sum of bytes_acked:"
-        << congestion_event->bytes_acked
-        << " and bytes_lost:" << congestion_event->bytes_lost;
+    QUIC_LOG(ERROR) << "prior_bytes_in_flight:"
+                    << congestion_event->prior_bytes_in_flight
+                    << " is smaller than the sum of bytes_acked:"
+                    << congestion_event->bytes_acked
+                    << " and bytes_lost:" << congestion_event->bytes_lost;
     congestion_event->bytes_in_flight = 0;
   }
 
