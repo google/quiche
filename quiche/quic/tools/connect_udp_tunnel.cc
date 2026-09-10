359bc9849b0c998a89bc4411d3dd2e70b34c53a6
apaseltiner <apaseltiner@google.com>
2026-09-10 12:09:54 -0700

    Migrate users to tag-based structured_headers::Item constructors
    Use tag-based constructors (`Item::string` and `Item::token`) introduced in cl/978581202.
    
    This contains no intentional observable behavioral changes.
    
    Tested:
        TAP --sample ran all affected tests and none failed
        http://test/OCL:979214341:BASE:979159221:1789055904004:a0c33791
    PiperOrigin-RevId: 979320705

--- quiche/quic/tools/connect_udp_tunnel.cc
+++ quiche/quic/tools/connect_udp_tunnel.cc
@@ -383,11 +383,14 @@ void ConnectUdpTunnel::SendErrorResponse(absl::string_view status,
   quiche::HttpHeaderBlock headers;
   headers[":status"] = status;
 
-  structured_headers::Item proxy_status_item(server_label_);
+  structured_headers::Item proxy_status_item(structured_headers::Item::string,
+                                             server_label_);
+  // TODO(apaseltiner): RFC 9209, Section 2.1.1 specifies that `error` should be
+  // a token, not a string.
   structured_headers::Item proxy_status_error_item(
-      std::string{proxy_status_error});
+      structured_headers::Item::string, proxy_status_error);
   structured_headers::Item proxy_status_details_item(
-      std::string{error_details});
+      structured_headers::Item::string, error_details);
   structured_headers::ParameterizedMember proxy_status_member(
       std::move(proxy_status_item),
       {{"error", std::move(proxy_status_error_item)},
